d2d80d19a15171e65e511ced262aaba6bc7085f8
fisher <zhengxinlin@gmail.com>
2018-01-12 02:06:56 +0800

    test: add nginx parser case

---
+++ test/common/nginx_config_parser.test.js
@@ -0,0 +1,28 @@
+'use strcit';
+
+const should = require('should');
+const fs = require('fs');
+const path = require('path');
+const TestMod = require('../../common/nginx_config_parser');
+
+describe('nginx config parser', () => {
+  let list = fs.readdirSync(path.join(__dirname, './nginx_cases'));
+
+  describe('parse()', () => {
+    list.forEach((file) => {
+      if (!/\.conf$/.test(file)) {
+        return;
+      }
+      let confFile = path.join(__dirname, '/nginx_cases/' + file);
+      let confResult = path.join(__dirname, '/nginx_cases/' + file + '.json');
+      it('should parse fine:' + file, () => {
+        let cnt = fs.readFileSync(confFile).toString();
+        let tokens = TestMod.parse(cnt, {loc: false});
+      });
+      it('should parse fine with loc:' + file, () => {
+        let cnt = fs.readFileSync(confFile).toString();
+        let tokens = TestMod.parse(cnt, {loc: true});
+      });
+    });
+  });
+});
