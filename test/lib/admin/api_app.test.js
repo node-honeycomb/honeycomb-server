20cb46a446826c08087cdeac9639917d49f499e9
fisher <fishbar@users.noreply.github.com>
2018-08-22 15:05:44 +0800

    杂项修复 (#74)
    * optimize: gather_usage performance
    * deps: update deps
    * fix: getTargetSock() empty means all workers are running

--- test/lib/admin/api_app.test.js
+++ test/lib/admin/api_app.test.js
@@ -9,7 +9,7 @@ const supertest = require('supertest');
 const ip = require('ip').address();
 const appsPkgBase = path.join(__dirname, '../../../example-apps');
 
-describe('api app test: ', () => {
+describe('api_app.test.js', () => {
   let ips = '127.0.0.1';
   let agent = supertest(`http://localhost:${config.admin.port}`);
 
--- test/lib/admin/api_log.test.js
+++ test/lib/admin/api_log.test.js
@@ -10,7 +10,7 @@ const supertest = require('supertest');
 const ip = require('ip').address();
 const appsPkgBase = path.join(__dirname, '../../../example-apps');
 
-describe('api log test: ', () => {
+describe('api_log.test.js', () => {
   let ips = '127.0.0.1';
   let agent = supertest(`http://localhost:${config.admin.port}`);
 
