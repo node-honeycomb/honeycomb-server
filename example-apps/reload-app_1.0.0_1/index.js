c94369e291cc384b0ff3c8c5ec07393d1297db3d
fisher <zhengxinlin@gmail.com>
2018-09-29 14:12:03 +0800

    fix: reload app bug: killing old worker bug; reload config bug

--- example-apps/reload-app_1.0.0_1/index.js
+++ example-apps/reload-app_1.0.0_1/index.js
@@ -5,17 +5,16 @@ let path = require('path');
 let serverConfig = JSON.parse(process.env.HC_APP_CONFIG);
 
 let serv = http.createServer(function (req, res) {
-  res.end('simple-app_v1.1.0_1');
+  res.setHeader('content-type', 'application/json');
+  res.end(JSON.stringify(serverConfig.config));
 });
 
-console.log(serverConfig.config);
-
 exports.run = function (cb) {
-  let sock = serverConfig.targetSock || path.join(__dirname, '../../simple-app_v1.1.0_1.sock');
+  let sock = serverConfig.targetSock;
   serv.listen(sock);
   cb(null, {
     bind: '6001',
-    router: '/example',
+    router: '/reload-app',
     target: sock
   });
 };
