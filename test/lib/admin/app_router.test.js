b2a25d80fc2a453f1ee1fe37ddddca57063f63c1
fisher <zhengxinlin@gmail.com>
2019-08-03 16:13:54 +0800

    test

--- test/lib/admin/app_router.test.js
+++ test/lib/admin/app_router.test.js
@@ -9,7 +9,7 @@ const supertest = require('supertest');
 const ip = require('ip').address();
 const appsPkgBase = path.join(__dirname, '../../../example-apps');
 
-describe.only('app_router.test.js', () => {
+describe('app_router.test.js', () => {
   let ips = '127.0.0.1';
   let agent = supertest(`http://localhost:${config.admin.port}`);
 
